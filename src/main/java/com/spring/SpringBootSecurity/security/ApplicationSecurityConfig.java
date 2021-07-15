f0d094377f79faddf6a7879c53e4a6a81cb5cb99
khaled Mohamed Abdelghany <khaled.m.abdelghany@gmail.com>
2021-07-15 12:48:08 +0200

    Create an InMemory user with BCryptPasswordEncoder

--- src/main/java/com/spring/SpringBootSecurity/security/ApplicationSecurityConfig.java
+++ src/main/java/com/spring/SpringBootSecurity/security/ApplicationSecurityConfig.java
@@ -1,13 +1,28 @@
 package com.spring.SpringBootSecurity.security;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 
 @Configuration
 @EnableWebSecurity
 public class ApplicationSecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+
+    public ApplicationSecurityConfig(PasswordEncoder passwordEncoder) {
+        this.passwordEncoder = passwordEncoder;
+    }
+
 
     @Override
     protected void configure(HttpSecurity http) throws Exception{
@@ -27,5 +42,34 @@ public class ApplicationSecurityConfig extends WebSecurityConfigurerAdapter {
                 //has the authentication type to be Basic
                 .httpBasic();
 }
+    /*
+    steps to create a User:
+        1- @override the method userDetailsService and make it a @Bean
+            UserDetailsService is used to retrieve users from database
+        2- create the required user inside as shown of type UserDetails
+        3- return new InMemoryUserDetailsManager(user1,user2,......userN);
+            as it is the class of interest that implement USerDetailsService interface.
+            ----> if you try now run project you get Exception of "There is no PasswordEncoder mapped for the id "null" ??????????!!!!
+            ----> next is how to solve this
+        4- create class PasswordConfig to add password Encryptor act like @Configuration
+        5- make @Bean method that return the required PasswordEncoder
+        6- return the required encoder
+        7- inject passwordEncoder in our class "ApplicationSecurityConfig" and make it @Autowired
+        8- make a constructor for it.
+
+     */
+
+    @Override
+    @Bean
+    protected UserDetailsService userDetailsService(){
+        UserDetails ahmedUser = User.builder()
+                .username("ahmed")
+                .password(passwordEncoder.encode("password"))
+                .roles("Student")
+                .build();
+        return new InMemoryUserDetailsManager(
+                ahmedUser
+        );
+    }
 
 }
